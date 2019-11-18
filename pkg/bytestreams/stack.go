8f9b2e30479becaa31dec2abce9a93febcd15cb8
Pavel Petrukhin <pp@kodix.ru>
2019-11-19 01:59:20 +0300

    add comment docs

--- pkg/bytestreams/stack.go
+++ pkg/bytestreams/stack.go
@@ -2,11 +2,14 @@ package bytestreams
 
 import "io"
 
+// StackWriter describes a layered set of writers, which can be used to pipeline multiple bytestream handlers.
+// It handles not only data pipelining, but .Close and .Flush calls too.
 type StackWriter struct {
 	wr io.Writer
 	config
 }
 
+// NewStackWriter creates a new stacked writer with provided target writer and optional configuration.
 func NewStackWriter(wr io.Writer, options ...Option) *StackWriter {
 	var stack = &StackWriter{
 		wr: wr,
@@ -22,6 +25,9 @@ func NewStackWriter(wr io.Writer, options ...Option) *StackWriter {
 	return stack
 }
 
+// Push puts middlewares on top of the writer stack.
+// If wrapped writers impleement io.Closer or FLusher,
+// then corresponding methods will be pushed too callback stack too.
 func (stack *StackWriter) Push(wrappers ...Middleware) *StackWriter {
 	for _, wrap := range wrappers {
 		var wr = wrap(stack.wr)
@@ -36,10 +42,16 @@ func (stack *StackWriter) Push(wrappers ...Middleware) *StackWriter {
 	return stack
 }
 
+// Flush calls stacked .Flush methods, in reverse order of pushing of middlewares.
+// Error handling policy depends on special optional callback, .Flush method will return
+// a first occured error by default.
 func (stack *StackWriter) Flush() error {
 	return stack.onFlush()
 }
 
+// Close calls stacked .Close methods in reverse order of pushing of middlewares.
+// Error handling policy depends on special optional callback, .Close method will return
+// a first occured error by default.
 func (stack *StackWriter) Close() error {
 	return stack.onClose()
 }
